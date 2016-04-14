8c213f846204f6e0484ed38c0efc2664252445b7
Isabella <irammala@ska.ac.za>
2016-04-14 15:24:53 +0200

    modified scatter.py

--- scatter.py
+++ scatter.py
@@ -5,7 +5,7 @@ import numpy as np
 def scatter(prof,dm,period,freq):
     """
     Function to perform convolution for a scattered profile. If dm = 0, the function
-    returns the profile without convolution. 
+    returns the input profile without convolution. 
     
     Args:
     -----
@@ -19,12 +19,13 @@ def scatter(prof,dm,period,freq):
     --------
 
         conv   : convolution of the profile.
+
     """
 
     if dm != 0:
         
-        npatches = float(prof.shape[0])
-        tbin = (period * 1000) / npatches    # tbin = time of each bin in ms.
+        npts = float(prof.shape[0])
+        tbin = (period * 1000) / npts    # tbin = time of each bin in ms.
         log_tau = -6.46 + 0.154 * np.log10(dm) + 1.07 * (np.log10(dm))**2 - 3.86 * np.log10(freq)
 #       tau = scattering time scale as in Bhat et al. (2004) in ms.
         tau = 10**log_tau
@@ -37,9 +38,9 @@ def scatter(prof,dm,period,freq):
            ", period = " + str(period) + ", freq = " + str(freq) + ", tbin = " + str(tbin)
         
 
-        if bincrit > 10 * npatches: bincrit = 10 * npatches
+        if bincrit > 10 * npts: bincrit = 10 * npts
 
-        if bincrit < npatches: bincrit = npatches
+        if bincrit < npts: bincrit = npts
 
         if verbose is 'y': print "Critical bin: " + str(bincrit)
         
@@ -53,30 +54,31 @@ def scatter(prof,dm,period,freq):
         tb = 0
         tbf = 0
         
-        conv = np.zeros(int(npatches))
-        scr = []
-        while tb < bincrit + npatches - 1:
+        conv = np.zeros(int(npts))
+        #scr = np.zeros(int(npts))
+        while tb < bincrit + npts - 1:
 
-            #print tbf, conv.shape, npatches, bincrit+npatches-1
-            for i in np.arange(int(npatches) - 1):
+            #print tbf, conv.shape, npts, bincrit+npts-1
+            #the loops slows down the program
+            for i in np.arange(int(npts) - 1):
                
-                if tb + i - 2 > npatches:
-                    frac = (tb + i - 2 - npatches) * tbin / tau
-                    scr.append(np.exp(-frac))
+                if tb + i - 1 > npts:
+                    frac = (tb + i - 1 - npts) * tbin / tau
+                    scr = np.exp(-1.*frac)
                 else: 
-                    scr.append(0.)
+                    scr = 0.
 
-                conv[tbf] += tprof[i] * scr[i]
+                #conv[tbf] = tprof[i] * scr[i]
+                conv[tbf] += tprof[i] * scr
 
             tb += 1
             tbf += 1
-            if tbf > int(npatches) - 1: tbf = 0 
-
-        conv = conv[:]         
+            if tbf > int(npts) - 1: tbf=1 
+       
     
     if dm == 0.: 
         print('No scattering for dm == 0; returning the profile without convolution!')
-
+        conv = prof 
     return conv
 
 ###############################################################################################
@@ -84,7 +86,8 @@ def scatter(prof,dm,period,freq):
 
 if __name__ == "__main__":
     
-    prof = np.arange(12)
-    print scatter(prof,2.,0.5,1.4)
-    
+    prof = np.arange(200)
+    aa = scatter(prof,200,0.5,0.3)
+    print aa
+    print len(aa)
     
