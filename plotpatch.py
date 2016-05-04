0acb9971c73e1b67eed05f7949864bc77b9b0314
Isabella <irammala@ska.ac.za>
2016-05-04 12:17:43 +0200

    update

--- plotpatch.py
+++ plotpatch.py
@@ -1,15 +1,18 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import d2r
-import patch_width
+import patchwidth
+import los
+import patchcenter
 
-
-def plotpatch(P, hmin, hmax, npatch):
-    """Function to plot the patches for a given height range.
+def plotpatch(P, alpha, beta, hmin, hmax, npatch):
+    """Function to plot the patches for a given height range. Using a 2d gaussian
     
        Args:
        -----
        P       : rotational period (seconds)
+       alpha   : inclination angle (degrees)
+       beta    : impact parameter (degrees)
        hmin    : minimum emission height (in km)
        hmax    : maximum emission height (in km)
        npatch  : number of emission patches
@@ -18,50 +21,72 @@ def plotpatch(P, hmin, hmax, npatch):
        --------
        A plot of the patches projected on to observational plane.
     
-    """
+    """    
     
-    angle = np.linspace(-180, 180, num=50, endpoint=True)
-    patchwidths = patch_width.patch_width(P, hmin, hmax)
-    
+#   make a 2D array:
+    x = np.linspace(-180, 180, num=500, endpoint=True)
+    y = np.linspace(-180, 180, num=500, endpoint=True)
+    X,Y = np.meshgrid(x,y)
+
+#   patchcenter or mean of the plot from patch center:
+    cx, cy = patchcenter.patch_center(P, hmin, hmax, npatch)
+
 #   choose random patch widths (wp) depending on how patches specify:
-    np.random.shuffle(patchwidths)
-    wp = patchwidths[0:npatch]
+    patchwidths = patchwidth.patch_width(P, hmin, hmax)
+    #np.random.shuffle(patchwidths)
+    if npatch > 1:
+        np.random.shuffle(patchwidths)
+        wp = patchwidths[0:npatch]
+
+    if npatch == 1:
+        wp = patchwidths 
     
-    #x0 = wp[0] * d2r.cosD(angle)
-    #y0 = wp[0] * d2r.sinD(angle)
-    #x1 = wp[1] * d2r.cosD(angle)
-    #y1 = wp[1] * d2r.sinD(angle)
-    #for i in np.arange(len(wp)):
-    #    x = wp[i] * d2r.cosD(angle)
-    #    y = wp[i] * d2r.sinD(angle)  
-    X = []
-    Y = []
+
+#   wp == the spread (sigma)
+#   project the sigmax and sigmay to the line of sight plane:
+    xlos, ylos, thetalos = los.los(alpha, beta)
+    sigmax = wp * d2r.sinD(thetalos)
+    sigmay = wp * d2r.cosD(thetalos)
     
-    for i in np.arange(len(wp)):
-        X.append(wp[i] * d2r.cosD(angle))
-        Y.append(wp[i] * d2r.sinD(angle))
     
-    x0 = X[0]
-    y0 = Y[0]
-    x1 = X[1]
-    y1 = Y[1]
-    x2 = X[2]
-    y2 = Y[2] 
-    #fig = plt.figure()
-    #axs = fig.gca()
-    #axs.set_xticks(np.arange(-90, 90, 5))
-    #axs.set_yticks(np.arange(-90, 90, 5))
-    plt.plot(x0, y0)
-    plt.plot(x1, y1)
-    plt.plot(x2, y2)
+#   2D gaussian:
+    peak = 5 # trial peak 
+    a = ((d2r.cosD(thetalos) ** 2) / (2 * sigmax ** 2)) + ((d2r.sinD(thetalos) ** 2) / (2 * sigmay ** 2))
+    b = (-(d2r.sinD(2 * thetalos)) / (4 * sigmax ** 2)) + ((d2r.sinD(2 * thetalos)) / (4 * sigmay ** 2))
+    c = ((d2r.sinD(thetalos) ** 2) / (2 * sigmax ** 2)) + ((d2r.cosD(thetalos) ** 2) / (2 * sigmay ** 2))    
+
+    if npatch > 1:
+        for i in np.arange(len(cx)):
+            Z = peak * np.exp(-(a[i] * (X - cx[i]) ** 2 - 2 * b[i] * (X - cx[i]) * (Y - cy[i]) + c[i] * (Y - cy[i]) ** 2))
+            
+
+    if npatch == 1:
+        Z = peak * np.exp(-(a * (X - cx) ** 2 - 2 * b * (X - cx) * (Y - cy) + c * (Y - cy) ** 2)) 
+
+#    if npatch == 0:
+#        Z = peak * np.exp(-(a * (X - cx) ** 2 - 2 * b * (X - cx) * (Y - cy) + c * (Y - cy) ** 2)) 
+        
+    plt.figure()    
+    plt.contour(X, Y, Z)
+    plt.xlim(-90, 90)
+    #plt.ylim(-50, 50)
     plt.grid()
-    plt.show()
+    
+    plt.figure()
+    plt.grid()
+    plt.imshow(Z)
+    
+    plt.show()    
+ 
+  
     
     
 ############################ simple test ##########################
 if __name__ == "__main__":
-    P = 0.16
-    hmin = 30
-    hmax = 990
-    npatch = 3
-    plotpatch(P, hmin, hmax, npatch)
+    P = 0.13
+    alpha = 60
+    beta = 10
+    hmin = 950
+    hmax = 1000
+    npatch = 1
+    plotpatch(P, alpha, beta, hmin, hmax, npatch)
