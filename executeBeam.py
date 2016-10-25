897c0b476b6597c9359dac7423069f03d28f2540
Isabella-Dineo <irammala@ska.ac.za>
2016-10-25 14:36:37 +0100

    Additional commands parser options

--- executeBeam.py
+++ executeBeam.py
@@ -1,26 +1,33 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import numpy as np
-import os, argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-f')
-parser.add_argument('-it', type = int, default = 1)
-parser.add_argument('-p', type = float, default = 1.0)
+import argparse, os
+
+parser = argparse.ArgumentParser(prog='Run the beam code multiple times.')
+parser.add_argument('-f', type=str, default='outFile')
+parser.add_argument('-it', type=int, default=1)
+parser.add_argument('-p', type=float, default=1.0)
+parser.add_argument('-nch', type=float, default=10)
+parser.add_argument('-min_freq', type=float, default=0.03)
+parser.add_argument('-bw', type=float, default=0.01)
+parser.add_argument('-snr', type=int, default=100)
 args = parser.parse_args()
 filename = args.f
 iterations = args.it
-period = args.p
+P = args.p
+fmin = args.min_freq
+bw = args.bw
+nch = args.nch
+snr = args.snr
+
 for i in range(iterations):
-    alpha = np.rad2deg(np.arccos(np.random.uniform()))  # inclination angle in degrees
-    beta = np.random.uniform(-20, 20)                   # impact parameter in degrees
-#    P = np.random.uniform(0.15, 1.5)                    # Period in seconds
-    P = period
-    fmin = 0.065                                        # minimum frequency in GHz
-    bw = 0.01                                           # channel bandwidth in GHz
-    nch = 10                                            # Number of channels
-    nc = 4					        # Number of components
-    npatch = 4                                          # Number of active patches
-    iseed = np.random.randint(0,90012410)
-    os.system('generate_profile.py -alpha %.3f -beta %.3f -p %.3f -min_freq %.3f -chbw %.3f -nch %d -nc %d -npatch %d -snr 10000 -iseed %d -outfile %s' \
-               % (alpha, beta, P, fmin, bw, nch, nc, npatch, iseed, filename ))
+    alpha = np.rad2deg(np.arccos(np.random.uniform()))
+    iseed = np.random.randint(0, 90012410)
+    beta = np.random.uniform(-20, 20)
+    nc = 4
+    npatch = 4
+    os.system('generate_profile.py -alpha %.3f -beta %.3f -p %.3f -min_freq %.3f -chbw %.3f -nch %d -nc %d -npatch %d -snr %f -iseed %d -outfile %s' \
+               % (alpha, beta, P, fmin, bw, nch, nc, npatch, snr, iseed, filename ))
+
     os.system('plot_profile.py -outfile %s' % filename)
+
